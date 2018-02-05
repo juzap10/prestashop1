a00ef87ad485c9a90a716f303cadf3a1782a903c
Juan David Zapata <juzap10@gmail.com>
2018-02-05 14:00:34 -0500

    WEB-538 Crear repositorio GIT - Enlazar archivos Prestashop
    Crear repositorio GIT - Enlazar archivos Prestashop

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', '');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'CuFLb667mMn5sGA7IEj6ZWN5xxVg0QxzsPyYd0tro5RbZPgF9j6VEvq7');
+define('_COOKIE_IV_', '6Wo9RVbz');
+define('_PS_CREATION_DATE_', '2018-02-05');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.17');
+define('_RIJNDAEL_KEY_', 'SDZWbG0Um7k7dObOmwhw4Jg7HiqSeA27');
+define('_RIJNDAEL_IV_', '3UMxPcv9q44PSrp9tehAXA==');
